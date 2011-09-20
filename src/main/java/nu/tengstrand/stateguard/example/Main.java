2fba75c93576b79e6a478a5f41d3ae2c401b40ba
unknown <joakim.tengstrand@.jaybis.local>
2011-09-20 08:42:57 +0200

    Improved readability.

--- src/main/java/nu/tengstrand/stateguard/example/Main.java
+++ src/main/java/nu/tengstrand/stateguard/example/Main.java
@@ -9,21 +9,21 @@ import nu.tengstrand.stateguard.example.book.BookCreator;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("----- Build: Missing binding ------");
-        BookBuilder bookBuilder = Book.build().title("My Book").pages(50);
+        System.out.println("----- Build: Missing attributes ------");
+        BookBuilder bookBuilder = Book.build().title("My Book");
         printMessage(bookBuilder);
 
-        System.out.println("----- Build: With binding = PAPERBACK ------");
-        bookBuilder.binding(BookBinding.PAPERBACK);
+        System.out.println("\n----- Build: With binding + pages ------");
+        bookBuilder.binding(BookBinding.PAPERBACK).pages(50);
         printMessage(bookBuilder);
         Book bookWithMissingBinding = bookBuilder.asValidState();
         System.out.println("Book: " + bookWithMissingBinding);
 
-        System.out.println("----- Create: Empty title ------");
+        System.out.println("\n----- Create: Empty title ------");
         BookCreator bookCreatorWithEmptyTitle = Book.create().title("").paperback().pages(100);
         printMessage(bookCreatorWithEmptyTitle);
 
-        System.out.println("----- Create: Valid book ------");
+        System.out.println("\n----- Create: Valid book ------");
         BookCreator bookCreator = Book.create().title("The book").paperback().pages(200);
         printMessage(bookCreator);
         Book book = bookCreator.asValidState();
